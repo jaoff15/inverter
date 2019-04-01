eb33b79cd9f394f42b727214351c738859c5ee5d
jaoff15 <jaoff15@student.sdu.dk>
2019-04-01 09:13:05 +0200

    Clark park code added

---
+++ clarkpark.c
@@ -0,0 +1,82 @@
+
+
+#include "headers/clarkpark.h"
+
+
+// Combined clark and park function. Performs both functions
+void clarkPark(double *iD, double *iQ, double iA, double iB, double iC, double angle){
+	// Clark
+	double iAlpha;
+	double iBeta;
+	clark(&iAlpha, &iBeta, iA, iB, iC);
+
+	// Park
+	double tmp_iD;
+	double tmp_iQ;
+	park(&tmp_iD, &tmp_iQ, iAlpha, iBeta, angle);
+
+	// Write to output
+	*iD = tmp_iD;
+	*iQ = tmpiQ;
+}
+
+
+// Combined inverse park and inverse clark function. Performs both functions
+void invClarkPark(double *iA, double *iB, double *iC, double iD, double iQ, double angle){
+	// Inverse Park
+	double iAlpha;
+	double iBeta;
+	invPark(&iAlpha, &iBeta, iD, iQ, angle);
+
+	// Inverse Clark
+	double tmp_iA;
+	double tmp_iB;
+	double tmp_iC;
+	invClark(&tmp_iA, &tmp_iB, &tmp_iC, iAlpha, iBeta);
+
+
+	// Write to output
+	*iA = tmp_iA;
+	*iB = tmp_iB;
+	*iC = tmp_iC;
+}
+
+
+
+
+// The Clark function
+void clark(double *iAlpha, double *iBeta, double iA, double iB, double iC){
+	*iAlpha = TWO_THIRDS * iA - ONE_THIRD * (iB - iC);
+	*iBeta  = TWO_OVER_SQRT_THREE * (iB - iC);
+}
+
+// The inverse Clark function
+void invClark(double *iA, double *iB, double *iC, double iAlpha, double iBeta){
+	*iA = iAlpha;
+	*iB = -HALF * iAlpha + SQRT_THREE_OVER_TWO * iBeta;
+	*iC = -HALF * iAlpha - SQRT_THREE_OVER_TWO * iBeta;
+}
+
+// The Park function
+void park(double *iD, double *iQ, double iAlpha, double iBeta, double angle){
+	const double cosAngle = cos(angle);
+	const double sinAngle = sin(angle);
+
+	*iD = iAlpha * cosAngle + iBeta * sinAngle;
+	*iQ = -iAlpha * sinAngle + iBeta * cosAngle;
+}
+
+// The inverse Park function
+void invPark(double *iAlpha, double *iBeta, double iD, double iQ, double angle){
+	const double cosAngle = cos(angle);
+	const double sinAngle = sin(angle);
+
+	*iAlpha = iD * cosAngle - iQ * sinAngle;
+	*iBeta  = iD * sinAngle + iQ * cosAngle;
+}
+
+
+
+
+
+
---
+++ main.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+#include "clarkpark.h"
+
+void main(){
+	// Measurements	
+	double angle = 0;
+	double iA = -0.809207777821378;
+	double iB = -0.104205583519848;
+	
+
+	double iC = 0.913413361341225;
+
+
+	double iD;
+	double iQ;
+	clarkPark(&iD, &iQ, iA, iB, iC, angle);
+	
+	double new_iA = 0;
+	double new_iB = 0;
+	double new_iC = 0;
+	invClarkPark(&new_iA, &new_iB, &new_iC, iD, iQ, angle);
+
+
+
+
+	printf("iA: %f\n",new_iA);
+	printf("iB: %f\n",new_iB);
+	printf("iC: %f\n",new_iC);
+
+
+}
+
+
