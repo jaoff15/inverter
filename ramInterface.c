6efe0f0c406edc74cbc5a930c634a269bb559576
jaoff15 <jaoff15@student.sdu.dk>
2019-04-01 11:11:25 +0200

    Read and write operations changed from 8 bit to 64 bit to account for the type double being 8 bytes

--- ramInterface.c
+++ ramInterface.c
@@ -9,7 +9,7 @@ void readMemory(addr_typ _addr, data_typ *data){
 	addr_typ addr = BASE_ADDRESS + (8 * _addr);
 
 	printf("Address: %lu \n",addr);
-	//Xil_In8(addr, data);
+	//Xil_In64(addr, data);
 }
 
 // Function to write to a specified memory location
@@ -17,5 +17,5 @@ void writeMemory(addr_typ _addr, data_typ data){
 	addr_typ addr = BASE_ADDRESS + (8 * _addr);
 
 	printf("Address: %lu \n",addr);
-	//Xil_Out8(addr, data);
+	//Xil_Out64(addr, data);
 }
