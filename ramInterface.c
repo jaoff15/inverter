8bd6445b0647f3d756a616aee23c85a759d9361a
jaoff15 <jaoff15@student.sdu.dk>
2019-04-01 11:03:45 +0200

    Idea for memory interface between PS and PL added

---
+++ ramInterface.c
@@ -0,0 +1,21 @@
+
+
+#include "ramInterface.h"
+
+
+
+// Function to read from a specified memory location
+void readMemory(addr_typ _addr, data_typ *data){
+	addr_typ addr = BASE_ADDRESS + (8 * _addr);
+
+	printf("Address: %lu \n",addr);
+	//Xil_In8(addr, data);
+}
+
+// Function to write to a specified memory location
+void writeMemory(addr_typ _addr, data_typ data){
+	addr_typ addr = BASE_ADDRESS + (8 * _addr);
+
+	printf("Address: %lu \n",addr);
+	//Xil_Out8(addr, data);
+}
